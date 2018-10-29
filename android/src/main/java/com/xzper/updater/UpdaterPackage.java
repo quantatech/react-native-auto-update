102aaec2e739e363503ed564e2e39dee3e9895ce
xzper <mail@xzper.com>
2018-10-29 18:10:32 +0800

    fix error

--- android/src/main/java/com/xzper/updater/UpdaterPackage.java
+++ android/src/main/java/com/xzper/updater/UpdaterPackage.java
@@ -6,29 +6,22 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
 
 public class UpdaterPackage implements ReactPackage {
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return Arrays.<NativeModule>asList(
+                new UpdaterModule(reactContext)
+        );
     }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Collections.emptyList();
-    }
-
-    @Override
-    public List<NativeModule> createNativeModules(
-            ReactApplicationContext reactContext) {
-        List<NativeModule> modules = new ArrayList<>();
-
-        modules.add(new UpdaterModule(reactContext));
-
-        return modules;
+        return Arrays.<ViewManager>asList();
     }
 }
